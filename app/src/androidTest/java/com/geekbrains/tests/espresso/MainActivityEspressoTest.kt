e4ef60e38826c8313f46aba95cb777775ece06a2
Ra53n <nikkon2001@mail.ru>
2022-11-26 14:05:39 +0500

    Добавил кнопку поиска и избавился от еспрессо в тесте

--- app/src/androidTest/java/com/geekbrains/tests/espresso/MainActivityEspressoTest.kt
+++ app/src/androidTest/java/com/geekbrains/tests/espresso/MainActivityEspressoTest.kt
@@ -1,4 +1,4 @@
-package com.geekbrains.tests
+package com.geekbrains.tests.espresso
 
 import android.view.View
 import androidx.test.core.app.ActivityScenario
@@ -9,13 +9,14 @@ import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.geekbrains.tests.BuildConfig
+import com.geekbrains.tests.R
 import com.geekbrains.tests.view.search.MainActivity
 import org.hamcrest.Matcher
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-import java.util.regex.Pattern
 
 @RunWith(AndroidJUnit4::class)
 class MainActivityEspressoTest {
